b265245ab8798522c0134f8ed63237906f694fe8
sponde25 <anirudh_jain@am.ism.ac.in>
2019-03-08 21:11:16 +0900

    Alexnet Linear Batchnorm

--- models/cifar/alexnet.py
+++ models/cifar/alexnet.py
@@ -34,9 +34,10 @@ class AlexNet(nn.Module):
         self.classifier = nn.Sequential(
             #nn.Dropout(),
             nn.Linear(256 * 2 * 2, 4096),
+            nn.BatchNorm1d(4096),
             nn.ReLU(inplace=True),
-            #nn.Dropout(),
             nn.Linear(4096, 4096),
+            nn.BatchNorm1d(4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
